08b2724aa5746e00caa5c5dd14af9d08ca3559ce
anmol.email <anmol.ag123@gmail.com>
2017-05-07 01:13:13 +0530

    ubounded knapsack problem solution for buying apples problem

--- Dynamic Programming/ABA12C.cpp
+++ Dynamic Programming/ABA12C.cpp
@@ -1,3 +1,5 @@
+/*Author--->Anmol Agarwal */
+
 #include<bits/stdc++.h>
 #define m 100005
 using namespace std;
@@ -8,7 +10,7 @@ int main(){
 	cin >> t;
 	while(t--){
 		
-		int a[100005];
+		int a[m];
 		cin >> n >> k;
 		
 		int dp[k+1];
@@ -20,8 +22,8 @@ int main(){
 		dp[0]=0;
 		for(int i=1;i<=k;i++){
 			for(int j=1;j<=i;j++){
-				if(a[j]!=-1 && dp[i-j]!=-1){
-					dp[i] = min(dp[i],dp[i-j]+a[j]);
+				if(a[j]!=-1 && dp[i-j]!=-1){  //dp[i-j] is checked as to eliminate the addition of unavailable packet in the nextpacket
+					dp[i] = min(dp[i],dp[i-j]+a[j]);   //Taking minimum of Current Packet's value or combination of previous packets 
 				}						
 			}
 		}
